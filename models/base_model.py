4039fd9f5dfa4c80925060e33157ad85d4424e8c
mdallen5393 <allen_matthew@outlook.com>
2022-09-30 11:35:21 -0700

    Task 4. BaseModel

--- models/base_model.py
+++ models/base_model.py
@@ -32,7 +32,7 @@ class BaseModel:
         `__dict__` of the instance.
         """
         myDict = self.__dict__.copy()
-        myDict['__class__'] = self.__class__
+        myDict['__class__'] = type(self).__name__
         myDict['created_at'] = self.created_at.isoformat()
         myDict['updated_at'] = self.created_at.isoformat()
         return myDict
