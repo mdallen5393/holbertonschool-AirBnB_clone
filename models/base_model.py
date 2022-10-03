5ac42adbabf45d3067e9eb72f3bffe9b599f8466
mdallen5393 <allen_matthew@outlook.com>
2022-10-03 12:26:25 -0700

    updated base model

--- models/base_model.py
+++ models/base_model.py
@@ -4,7 +4,7 @@
 """
 from datetime import datetime
 import uuid
-import models
+# import models
 
 
 class BaseModel:
@@ -25,7 +25,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = self.created_at
-            models.storage.new(self)
+            # models.storage.new(self)
 
     def __str__(self):
         """
@@ -40,7 +40,7 @@ class BaseModel:
         with the current datetime.
         """
         self.updated_at = datetime.now()
-        models.storage.save(self)
+        # models.storage.save(self)
 
     def to_dict(self):
         """
