8fc5e404377ed8bee3e6b1745089db60681101ec
mdallen5393 <allen_matthew@outlook.com>
2022-10-03 12:13:28 -0700

    Updated base_model

--- models/base_model.py
+++ models/base_model.py
@@ -26,6 +26,7 @@ class BaseModel:
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
             models.storage.new(self)
+            models.storage.save()
 
     def __str__(self):
         """
