b8efbca1cc8ae278461263d9c3c25cf2023d1017
mdallen5393 <allen_matthew@outlook.com>
2022-10-03 12:24:14 -0700

    updated base model

--- models/base_model.py
+++ models/base_model.py
@@ -20,11 +20,11 @@ class BaseModel:
                     setattr(self, key, value)
             s = '%Y-%m-%dT%H:%M:%S.%f'
             self.created_at = datetime.strptime(self.created_at, s)
-            self.updated_at = self.created_at
+            self.updated_at = datetime.strptime(self.updated_at, s)
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
-            self.updated_at = datetime.now()
+            self.updated_at = self.created_at
             models.storage.new(self)
 
     def __str__(self):
